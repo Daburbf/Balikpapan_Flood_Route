67af05ba49dfc10694052d90f9faf8a3c04c97a4
Daburbf <11241024@student.itk.ac.id>
2025-12-14 22:49:35 +0800

    .

--- Balikpapan_Flood_Route/core/dijkstra.py
+++ Balikpapan_Flood_Route/core/dijkstra.py
@@ -2,17 +2,16 @@ import heapq
 
 class Dijkstra:
     def __init__(self, graph):
-        self.graph = graph.G  # Mengambil object graph NetworkX
+        self.graph = graph.G  
 
     def find_route(self, start_lat, start_lon, end_lat, end_lon):
-        # 1. Cari Node terdekat dari koordinat input
+
         start_node = self.get_nearest_node(start_lat, start_lon)
         end_node = self.get_nearest_node(end_lat, end_lon)
 
         if not start_node or not end_node:
             return None
 
-        # 2. Algoritma Dijkstra Standard
         queue = [(0, start_node)]
         distances = {start_node: 0}
         previous_nodes = {start_node: None}
@@ -44,39 +43,32 @@ class Dijkstra:
         if not target_found:
             return None
 
-        # 3. Rekonstruksi Jalur (Backtracking)
         path_nodes = []
         curr = end_node
         while curr is not None:
             path_nodes.append(curr)
             curr = previous_nodes[curr]
-        path_nodes.reverse() # Balik urutan agar dari Start -> End
+        path_nodes.reverse()
 
-        # --- BAGIAN PENTING: MENGAMBIL GEOMETRI (LEKUKAN) ---
         route_coords = []
         
         for i in range(len(path_nodes) - 1):
             u = path_nodes[i]
             v = path_nodes[i+1]
-            
-            # Ambil data jalan antara titik U dan V
+
             edge_data = self.graph.get_edge_data(u, v)[0]
             
-            # Cek apakah jalan ini punya data 'geometry' (lekukan detail)
             if 'geometry' in edge_data:
-                # Geometry formatnya (Lon, Lat), kita butuh (Lat, Lon)
-                # coords list otomatis ada jika pakai OSMnx
+            
                 geo_coords = list(edge_data['geometry'].coords)
                 
-                # Tukar posisi dari (Lon, Lat) ke (Lat, Lon)
                 for lon, lat in geo_coords:
                     route_coords.append([lat, lon])
             else:
-                # Jika tidak ada lekukan (jalan lurus), ambil titik ujungnya saja
+
                 node_data = self.graph.nodes[u]
                 route_coords.append([node_data['y'], node_data['x']])
-        
-        # Tambahkan titik terakhir
+    
         last_node = self.graph.nodes[path_nodes[-1]]
         route_coords.append([last_node['y'], last_node['x']])
 
