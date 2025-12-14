5f3e003f0dfb20e8d02d4b0904aada315c2c784e
Daburbf <11241024@student.itk.ac.id>
2025-12-14 22:50:28 +0800

    .

--- Balikpapan_Flood_Route/core/place_loader.py
+++ Balikpapan_Flood_Route/core/place_loader.py
@@ -7,10 +7,9 @@ def load_places_from_csv(filename="tempat_balikpapan.csv"):
     Format: {"Nama Tempat": (Latitude, Longitude)}
     """
     places_dict = {}
-    
-    # Cek apakah file ada
+
     if not os.path.exists(filename):
-        print(f"⚠️ File {filename} tidak ditemukan. Menggunakan data kosong.")
+        print(f"File {filename} tidak ditemukan. Menggunakan data kosong.")
         return places_dict
 
     try:
@@ -21,16 +20,13 @@ def load_places_from_csv(filename="tempat_balikpapan.csv"):
                 lat = float(row['lat'])
                 lon = float(row['lon'])
                 category = row['kategori']
-                
-                # Kita gabungkan nama + kategori agar lebih jelas
-                # Contoh: "ATM BNI (ATM)"
+         
                 full_name = f"{name} ({category})"
-                
-                # Simpan ke dictionary
+               
                 places_dict[full_name] = (lat, lon)
                 
-        print(f"✅ Berhasil memuat {len(places_dict)} tempat dari CSV.")
+        print(f"Berhasil memuat {len(places_dict)} tempat dari CSV.")
     except Exception as e:
-        print(f"❌ Gagal membaca CSV: {e}")
+        print(f"Gagal membaca CSV: {e}")
         
     return places_dict
